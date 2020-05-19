449b6cbf786ceabcee0ad7c299b97dfa12429baf
suziesta <sujatapatil92@gmail.com>
2020-05-19 14:35:39 -0500

    stack implementation using linkedlist

---
+++ Stacks/Stack Implementation using linkedlist/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Stack s=new Stack();
+	    s.push(10);
+	    s.push(20);
+	    s.push(3);
+        System.out.println(s.pop());
+    }
+}
