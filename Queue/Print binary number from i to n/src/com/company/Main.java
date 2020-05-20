d1a2b3e06555fc900d418355a8e5bede68b1d08e
suziesta <sujatapatil92@gmail.com>
2020-05-20 10:37:14 -0500

    modified mai file of binary number printing

--- Queue/Print binary number from i to n/src/com/company/Main.java
+++ Queue/Print binary number from i to n/src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    printBinary(10);
+    }
+
+    public static void printBinary(int n){
+        Deque<String> q = new ArrayDeque<>();
+        q.add("1");
+
+        for(int i=0; i<n; i++){
+            String x = q.remove();
+            q.add(x+"0");
+            q.add(x+"1");
+            System.out.println(x);
+        }
     }
 }
