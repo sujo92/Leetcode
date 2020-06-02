712319e1e1b181ad08d844375b3a2427dd28b537
Sujata Patil <sujatapatil2010@gmail.com>
2020-06-02 17:57:39 -0500

    folder structure changes

--- Sorting algorithm/src/InsertionSort.java
+++ Sorting algorithm/src/InsertionSort.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = new int[]{4,3,2,10,12,1,5,6};
+        sort(arr);
+        for(int a: arr){
+            System.out.println(a);
+        }
+    }
+
+    public static void sort(int[] arr) {
+        int len = arr.length;
+
+        for(int i=1; i<len; i++){
+            int currIndex= i;
+            while(currIndex > 0 && arr[currIndex] < arr[currIndex-1]){
+                int temp= arr[currIndex-1];
+                arr[currIndex-1] = arr[currIndex];
+                arr[currIndex] = temp;
+                currIndex--;
+            }
+        }
+    }
 }
