1b4c14de734b84699aaec5ced55ea31e6d59c202
Sujata Patil <sujatapatil2010@gmail.com>
2020-06-05 10:02:31 -0500

    merge sort updated

--- Sorting algorithm/src/MergeSort.java
+++ Sorting algorithm/src/MergeSort.java
@@ -1,8 +1,11 @@
 public class MergeSort {
     public static void main(String[] args) {
+        
         int[] arr= new int[]{4,3,2,10,12,1,5,6};
         int[] merged = new int[arr.length];
+
         sort(arr, 0 , arr.length-1, merged);
+
         for(int a: merged){
             System.out.println(a);
         }
